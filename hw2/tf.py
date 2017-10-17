8372a714f8da3e9939fdc038351cb3c71f25adb6
Sean Liao <seankhliao@gmail.com>
2017-10-17 23:23:38 +0800

    deeper models

--- hw2/tf.py
+++ hw2/tf.py
@@ -7,34 +7,37 @@ import tensorflow as tf
 tf.logging.set_verbosity(tf.logging.INFO)
 
 STEPS = (254 * 10000)
-VERSION = 3
+VERSION = 4
 
 feature_columns = [tf.feature_column.numeric_column("x", shape=[106])]
 
 linear = tf.estimator.LinearClassifier(feature_columns=feature_columns,
-                                       model_dir="model/tf-wide-"+str(VERSION),
+                                       model_dir="model/tf-wide-" +
+                                       str(VERSION),
                                        n_classes=2)
 
-dnn = tf.estimator.DNNClassifier(hidden_units=[20],
+dnn = tf.estimator.DNNClassifier(hidden_units=[128, 32],
                                  feature_columns=feature_columns,
-                                 model_dir="model/tf-deep-" +str(VERSION),
+                                 model_dir="model/tf-deep-" + str(VERSION),
                                  n_classes=2)
 
-linednn = tf.estimator.DNNLinearCombinedClassifier(model_dir="model/tf-dw-"+str(VERSION),
+linednn = tf.estimator.DNNLinearCombinedClassifier(model_dir="model/tf-dw-" + str(VERSION),
                                                    linear_feature_columns=feature_columns,
                                                    dnn_feature_columns=feature_columns,
-                                                   dnn_hidden_units=[40],
+                                                   dnn_hidden_units=[128, 32],
                                                    n_classes=2)
 
 
 def train(X_train, Y_train, est, steps):
-    x_d = np.genfromtxt(X_train, dtype=np.float32, skip_header=1, delimiter=',')
+    x_d = np.genfromtxt(X_train, dtype=np.float32,
+                        skip_header=1, delimiter=',')
     x = {"x": x_d}
     y = np.genfromtxt(Y_train, dtype=np.float32, skip_header=1, delimiter=',')
 
-
-    train_fn = tf.estimator.inputs.numpy_input_fn(x=x,y=y, shuffle=True, num_epochs=None)
-    eval_fn = tf.estimator.inputs.numpy_input_fn(x=x,y=y, num_epochs=1, shuffle=False)
+    train_fn = tf.estimator.inputs.numpy_input_fn(
+        x=x, y=y, shuffle=True, num_epochs=None)
+    eval_fn = tf.estimator.inputs.numpy_input_fn(
+        x=x, y=y, num_epochs=1, shuffle=False)
 
     if est == "linear":
         linear.train(train_fn, steps=1000000)
@@ -45,6 +48,7 @@ def train(X_train, Y_train, est, steps):
     elif est == "linednn":
         linednn.train(train_fn, steps=1000000)
         print(linednn.evaluate(eval_fn))
+
 
 def test(X_test, est, outputFile):
     t_d = np.genfromtxt(X_test, dtype=np.float32, skip_header=1, delimiter=',')
@@ -63,10 +67,8 @@ def test(X_test, est, outputFile):
     with open(outputFile, 'w') as of:
         w = csv.writer(of)
         w.writerow(["id", "label"])
-        for i,x in enumerate([p["class_ids"].tolist() for p in arr]):
-            w.writerow([i+1, x[0]])
-
-
+        for i, x in enumerate([p["class_ids"].tolist() for p in arr]):
+            w.writerow([i + 1, x[0]])
 
 
 if __name__ == "__main__":
@@ -76,7 +78,7 @@ if __name__ == "__main__":
     if mode == "train":
         X = sys.argv[3]
         Y = sys.argv[4]
-        train(X,Y, est, STEPS)
+        train(X, Y, est, STEPS)
     else:
         T = sys.argv[3]
         outputFile = sys.argv[4]
